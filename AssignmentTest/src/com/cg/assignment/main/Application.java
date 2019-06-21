3d4e5eaec8ab9c2d4a79e663fa6ec34e787e702d
akuma835 <amit.df.kumar@capgemini.com>
2019-06-21 12:08:08 +0530

    Number String Changed

--- AssignmentTest/src/com/cg/assignment/main/Application.java
+++ AssignmentTest/src/com/cg/assignment/main/Application.java
@@ -14,7 +14,7 @@ public class Application {
 		float secondNumber = 0;
 		float resultDivision = 0;
 		try {
-			System.out.println("Enter the FirstNumber");
+			System.out.println("Enter the First");
 			firstNumber = scanner.nextInt();
 			System.out.println("Enter the SecondNumber" + "");
 			secondNumber = scanner.nextInt();
